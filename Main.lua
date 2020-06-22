acd9a56d0b143ded3ec528ba55fb69f4fc9cb68c
JailedMist <65553073+JailedMist@users.noreply.github.com>
2020-06-22 15:37:47 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,4 +1,4 @@
-if secure_load and "Sentinel" then 
+if secure_load and "Sentinel" or pebc_execute and "ProtoSmasher" then 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/JailedMist/JailedHax/master/Sentinel", true))()
 else 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/JailedMist/JailedHax/master/Other", true))()
